6449676a17fe40471a1a34e6be7d61c6932ac34a
Francesco Matteo Depalma <157421031+FDMDeveloper@users.noreply.github.com>
2024-06-19 15:29:32 +0200

    Update page.tsx

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,18 +3,17 @@ import Container from '../components/Container';
 import FormWrap from '../components/FormWrap';
 import RegisterForm from './RegisterForm';
 import { getCurrentUser } from '@/actions/getCurrentUser';
-   
-export const forceStaticRendering = 'force-static';
-const Register = async() => {
+
+const Register = async () => {
     const currentUser = await getCurrentUser();
 
-    return ( 
-    <Container>
-        <FormWrap>
-            <RegisterForm currentUser={currentUser} />
-        </FormWrap>
-    </Container>
- );
-}
- 
+    return (
+        <Container>
+            <FormWrap>
+                <RegisterForm currentUser={currentUser} />
+            </FormWrap>
+        </Container>
+    );
+};
+
 export default Register;
