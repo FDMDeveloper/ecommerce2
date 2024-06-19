b6f22a88327ab01652621c7b6e2742138b2f6f86
Francesco Matteo Depalma <157421031+FDMDeveloper@users.noreply.github.com>
2024-06-19 14:12:31 +0200

    Update stripe-webhook.ts

--- pages/api/stripe-webhook.ts
+++ pages/api/stripe-webhook.ts
@@ -12,44 +12,43 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: "2024-04-10",
 });
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const buf = await buffer(req);
-  const sig = req.headers["stripe-signature"];
 
-  if (!sig) {
+  // Handle missing signature
+  if (!req.headers["stripe-signature"]) {
     res.status(400).send("Missing the stripe signature");
+    return;
   }
+
+  const sig = req.headers["stripe-signature"] as string; // Cast to string
 
   let event: Stripe.Event;
 
   try {
-    event = stripe.webhooks.constructEvent(
-      buf,
-      sig,
-      process.env.STRIPE_WEBHOOK_SECRET!
-    );
+    event = stripe.webhooks.constructEvent(buf, sig, process.env.STRIPE_WEBHOOK_SECRET!);
   } catch (error) {
-    res.status(400).send("Webhook error" + error);
+    res.status(400).send("Webhook error: " + error);
+    return; // Exit the function after sending the error response
   }
 
-  switch(event.type){
-    case 'charge.succeeded':
-        const charge: any = event.data.object as Stripe.Charge
+  // Now 'event' is guaranteed to be a Stripe.Event object
 
-        if(typeof charge.payment_intent === 'string'){
-            await prisma?.order.update({
-                where: {paymentIntentId: charge.payment_intent},
-                data: {status: 'complete', address: charge.shipping?.address},
-            });
-        }
-        break
-        default:
-            console.log('Unhanled event type' + event.type);
+  switch (event.type) {
+    case "charge.succeeded":
+      const charge: any = event.data.object as Stripe.Charge;
+
+      if (typeof charge.payment_intent === "string") {
+        await prisma?.order.update({
+          where: { paymentIntentId: charge.payment_intent },
+          data: { status: "complete", address: charge.shipping?.address },
+        });
+      }
+      break;
+    default:
+      console.log("Unhandled event type:", event.type);
   }
 
-  res.json({received: true});
-  
+  res.json({ received: true });
 }
+
