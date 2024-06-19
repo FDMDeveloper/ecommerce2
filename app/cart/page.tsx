dc4bca252b27f41664a6820a3b15daa5b3f71fed
Francesco Matteo Depalma <157421031+FDMDeveloper@users.noreply.github.com>
2024-06-19 15:26:23 +0200

    Update page.tsx

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -3,17 +3,16 @@ import Container from "../components/Container";
 import CartClient from './CartClient';
 import { getCurrentUser } from "@/actions/getCurrentUser";
 
-export const forceStaticRendering = 'force-static';
+const Cart = async () => {
+    const currentUser = await getCurrentUser();
 
-const Cart = async() => {
+    return (
+        <div className="pt-8">
+            <Container>
+                <CartClient currentUser={currentUser} />
+            </Container>
+        </div>
+    );
+};
 
-    const currentUser = await getCurrentUser()
-
-    return <div className="pt-8">
-        <Container>
-            <CartClient currentUser = {currentUser}/>
-        </Container>
-    </div>;
-}
- 
 export default Cart;
